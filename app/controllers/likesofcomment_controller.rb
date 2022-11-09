478ef8dec71819c4cf4262bff04a3724324f1c2d
An5H <dhimanansh7@gmail.com>
2022-11-09 19:54:38 +0530

    Removed unused controllers and views

--- app/controllers/likesofcomment_controller.rb
+++
@@ -1,2 +0,0 @@
-class LikesofcommentController < ApplicationController
-end
--- test/controllers/likesofcomment_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class LikesofcommentControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
